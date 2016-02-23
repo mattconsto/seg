c29352253041bb077bf7c9e55828d9e3181cb26b
Matthew Consterdine <mrconsterdine@googlemail.com>
2016-02-23 15:21:12 +0000

    Create build script

--- dashboard.bat
+++ dashboard.bat
@@ -1,1 +1,16 @@
+@echo off
+
+if not exist "bin" goto build
+
+:run
+echo Launching Dashboard
 java -cp .;bin;libs/* Launcher
+exit
+
+:build
+echo Compiling Dashboard
+mkdir bin
+dir /s /B *.java > sources.txt
+javac -cp .;libs/* -d bin -encoding UTF8 @sources.txt
+del sources.txt
+goto run
--- dashboard.sh
+++ dashboard.sh
@@ -1,4 +1,17 @@
-if [ `uname -o` = "Cygwin" ]; then
+if [ ! -d bin ]; then
+	echo "Compiling Dashboard"
+	mkdir bin
+	find -name *.java > sources.txt
+	if [ `uname -o` = Cygwin ]; then
+		javac -cp .\;libs/* -d bin -encoding UTF8 @sources.txt
+	else
+		javac -cp .:libs/* -d bin -encoding UTF8 @sources.txt
+	fi
+	rm sources.txt
+fi
+
+echo "Launching Dashboard"
+if [ `uname -o` = Cygwin ]; then
 	java -cp .\;bin\;libs/* Launcher
 else
 	java -cp .:bin:libs/* Launcher
