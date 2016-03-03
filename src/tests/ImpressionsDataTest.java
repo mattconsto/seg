a5fc768cd644773595237dc9a0b48cd339b8f4d5
Matthew Langford <ml2g14@soton.ac.uk>
2016-03-03 19:04:12 +0000

    Classpath now contains JUnit. Also some more for impressions.
    Former-commit-id: 59ba74f0b2bad9d3d776fd5717adb3656b0bb06f

--- src/tests/ImpressionsDataTest.java
+++ src/tests/ImpressionsDataTest.java
@@ -32,7 +32,6 @@ public class ImpressionsDataTest extends TestCase{
 	@Override
 	public void setUp()
 	{
-		System.out.println("Sup");
 		DatabaseConnection.setDbfile("Test");
 		filter = new Filter();
 		filter.setGender(FXCollections.observableArrayList("Any"));
@@ -42,12 +41,117 @@ public class ImpressionsDataTest extends TestCase{
 	}
 	
 	@Test
+	public void testFirstMinuteTotal() throws SQLException
+	{
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(39, data.get(0).getYValue());
+	}
+
+	
+	@Test
 	public void testFirstHourTotal() throws SQLException
 	{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(data.get(0).getYValue(),2126);
+		assertEquals(2126, data.get(0).getYValue());
 	}
 
+	@Test
+	public void testFirstDayTotal() throws SQLException
+	{
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(22049, data.get(0).getYValue());
+	}
+	
+	@Test
+	public void testFirstWeekTotal() throws SQLException
+	{
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(233947, data.get(0).getYValue());
+	}
+	
+	@Test
+	public void testFirstHourGender() throws SQLException
+	{
+		filter.setGender(FXCollections.observableArrayList("Female"));
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(1433, data.get(0).getYValue());
+		
+		filter.setGender(FXCollections.observableArrayList("Male"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(693, data.get(0).getYValue());
+	}
+	
+	@Test
+	public void testFirstHourAge() throws SQLException
+	{
+		filter.setAge(FXCollections.observableArrayList("Less than 25"));
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(415, data.get(0).getYValue());
+		
+		filter.setAge(FXCollections.observableArrayList("25 to 34"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(551, data.get(0).getYValue());
+		
+		filter.setAge(FXCollections.observableArrayList("35 to 44"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(520, data.get(0).getYValue());
+		
+		filter.setAge(FXCollections.observableArrayList("45 to 54"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(369, data.get(0).getYValue());
+		
+		filter.setAge(FXCollections.observableArrayList("Greater than 55"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(271, data.get(0).getYValue());
+	}
+	
+	@Test
+	public void testFirstHourIncome() throws SQLException
+	{
+		filter.setIncome(FXCollections.observableArrayList("Low"));
+		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(646, data.get(0).getYValue());
+		
+		filter.setIncome(FXCollections.observableArrayList("Medium"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(1051, data.get(0).getYValue());
+		
+		filter.setIncome(FXCollections.observableArrayList("High"));
+		impressionsConstructor = new ImpressionsGraphConstructor(filter);
+		
+		data = impressionsConstructor.fetchGraph().getData();
+		assertEquals(429, data.get(0).getYValue());
+	}
+	
+	@Test
+	public void testFirstHourContext() throws SQLException
+	{
+		
+	}
 }
