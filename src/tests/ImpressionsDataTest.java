b43af77a1ec6ddc45ac0008e2ed000e1a36fbbdf
Matthew Langford <ml2g14@soton.ac.uk>
2016-03-04 20:19:49 +0000

    Added header to test CSVs. Starting converting JUnits to test data values.
    Former-commit-id: 242a3a38eec557adcf50d1d7454e242dff9eef75

--- src/tests/ImpressionsDataTest.java
+++ src/tests/ImpressionsDataTest.java
@@ -24,7 +24,7 @@ public class ImpressionsDataTest extends TestCase{
 	@Override
 	public void setUp()
 	{
-		DatabaseConnection.setDbfile("Test.db");
+		DatabaseConnection.setDbfile("TestData.db");
 		filter = new Filter();
 		filter.setGender(FXCollections.observableArrayList("Any"));
 		filter.setAge(FXCollections.observableArrayList("Any"));
@@ -38,7 +38,7 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(39, data.get(0).getYValue());
+		assertEquals(1440, data.get(0).getYValue());
 	}
 
 	
@@ -48,7 +48,7 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(2126, data.get(0).getYValue());
+		assertEquals(2880, data.get(0).getYValue());
 	}
 
 	@Test
@@ -57,7 +57,7 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(22049, data.get(0).getYValue());
+		assertEquals(4320, data.get(0).getYValue());
 	}
 	
 	@Test
@@ -66,7 +66,7 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(233947, data.get(0).getYValue());
+		assertEquals(5760, data.get(0).getYValue());
 	}
 	
 	@Test
@@ -76,13 +76,13 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(1433, data.get(0).getYValue());
+		assertEquals(1440, data.get(0).getYValue());
 		
 		filter.setGender(FXCollections.observableArrayList("Male"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(693, data.get(0).getYValue());
+		assertEquals(1440, data.get(0).getYValue());
 	}
 	
 	@Test
@@ -92,31 +92,31 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(415, data.get(0).getYValue());
+		assertEquals(576, data.get(0).getYValue());
 		
 		filter.setAge(FXCollections.observableArrayList("25 to 34"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(551, data.get(0).getYValue());
+		assertEquals(576, data.get(0).getYValue());
 		
 		filter.setAge(FXCollections.observableArrayList("35 to 44"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(520, data.get(0).getYValue());
+		assertEquals(576, data.get(0).getYValue());
 		
 		filter.setAge(FXCollections.observableArrayList("45 to 54"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(369, data.get(0).getYValue());
+		assertEquals(576, data.get(0).getYValue());
 		
 		filter.setAge(FXCollections.observableArrayList("Greater than 55"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(271, data.get(0).getYValue());
+		assertEquals(576, data.get(0).getYValue());
 	}
 	
 	@Test
@@ -126,19 +126,19 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(646, data.get(0).getYValue());
+		assertEquals(960, data.get(0).getYValue());
 		
 		filter.setIncome(FXCollections.observableArrayList("Medium"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(1051, data.get(0).getYValue());
+		assertEquals(960, data.get(0).getYValue());
 		
 		filter.setIncome(FXCollections.observableArrayList("High"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(429, data.get(0).getYValue());
+		assertEquals(960, data.get(0).getYValue());
 	}
 	
 	@Test
@@ -148,36 +148,36 @@ public class ImpressionsDataTest extends TestCase{
 		ImpressionsGraphConstructor impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		ObservableList<XYChart.Data<String, Number>> data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(642, data.get(0).getYValue());
+		assertEquals(480, data.get(0).getYValue());
 		
 		filter.setContext(FXCollections.observableArrayList("Shopping"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(575, data.get(0).getYValue());
+		assertEquals(480, data.get(0).getYValue());
 		
 		filter.setContext(FXCollections.observableArrayList("Social Media"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(594, data.get(0).getYValue());
+		assertEquals(480, data.get(0).getYValue());
 		
 		filter.setContext(FXCollections.observableArrayList("Blog"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(315, data.get(0).getYValue());
+		assertEquals(480, data.get(0).getYValue());
 		
 		filter.setContext(FXCollections.observableArrayList("Hobbies"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(0, data.size());
+		assertEquals(480, data.size());
 		
 		filter.setContext(FXCollections.observableArrayList("Travel"));
 		impressionsConstructor = new ImpressionsGraphConstructor(filter);
 		
 		data = impressionsConstructor.fetchGraph().getData();
-		assertEquals(0, data.size());
+		assertEquals(480, data.size());
 	}
 }
