80d7704223f64fc8b10230ff8ff5a6af301dcf5c
Matthew Consterdine <mrconsterdine@googlemail.com>
2016-03-09 19:06:25 +0000

    Add terrible threading
    Former-commit-id: 0a13bd747c88870bd8b823bbebe220b3d497122e

--- src/dashboard/view/AuctionController.java
+++ src/dashboard/view/AuctionController.java
@@ -1,8 +1,6 @@
 package dashboard.view;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Date;
@@ -51,9 +49,8 @@ public class AuctionController extends AnchorPane {
 	@FXML private TableView<ObservableMetrics> tableResults;
 	@FXML private TableColumn<ObservableMetrics, String> metricCol;
 	@FXML private TableColumn<ObservableMetrics, String> resultCol;
-
-	final ObservableList<ObservableMetrics> tableContent = FXCollections.observableArrayList();
-	private Filter filter;
+	
+	private ObservableList<ObservableMetrics> tableMetrics = FXCollections.observableArrayList();
 	
 	@FXML private MenuItem deleteCampaign;
 	@FXML private MenuItem exportCampaign;
@@ -71,6 +68,8 @@ public class AuctionController extends AnchorPane {
 	@FXML private RadioButton rbByBounceTime;
 	@FXML private ToggleGroup grBounce;
 	@FXML private RadioButton rbByBouncePages;
+
+	private Filter filter;
 
 	public void setApp(Main application){
 		this.application = application;
@@ -97,12 +96,6 @@ public class AuctionController extends AnchorPane {
 		campaignName.setText(DatabaseConnection.getDbfile().replace(".db", ""));
 		generateGraph.setDisable(false);
 		//generateData(null);
-
-		try {
-			updateMetricsTable();
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
 	}
 	
 	private void configureFilters() {
@@ -119,7 +112,7 @@ public class AuctionController extends AnchorPane {
 	private final ListChangeListener<ObservableMetrics> tableSelectionChanged = (ListChangeListener.Change<? extends ObservableMetrics> c) -> {
 		ObservableMetrics item = tableResults.getSelectionModel().getSelectedItem();
 		if(item != null)
-			drawGraph(item.getDescription());
+			updateGraph(item.getDescription());
 	};
 
 	// Configure the table widget: set up its column, and register the
@@ -127,8 +120,7 @@ public class AuctionController extends AnchorPane {
 	private void configureTable() {
 		metricCol.setCellValueFactory(new PropertyValueFactory<>("description"));
 		resultCol.setCellValueFactory(new PropertyValueFactory<>("result"));
-		tableResults.setItems(tableContent);
-		assert tableResults.getItems() == tableContent;
+		tableResults.setItems(tableMetrics);
 
 		tableResults.getSelectionModel().getSelectedItems().addListener(tableSelectionChanged);
 	}
@@ -140,210 +132,6 @@ public class AuctionController extends AnchorPane {
 		DatabaseConnection.closeConnection();
 		application.getStage().close();
 	}
-
-	public void updateGraph(GraphConstructor graphConstructor, String yLabel, LineChart<Date, Number> lineChart){
-		try {
-			Series<Date, Number> data = graphConstructor.fetchGraph();
-			data.setName(yLabel);
-			lineChart.getData().add(data);			
-		} catch (SQLException e) {
-			System.err.println("Unable to fetch data from database: " + e.getMessage());
-		}
-	}
-
-	public void updateMetricsTable() throws SQLException{
-		tableContent.clear();
-		Connection conn = DatabaseConnection.getConnection();
-		ResultSet results = conn.createStatement().executeQuery("SELECT COUNT(*) AS Frequency, * FROM "
-				+ "(SELECT IMPRESSIONS.*, SERVER.* FROM IMPRESSIONS "
-				+ "INNER JOIN SERVER ON IMPRESSIONS.ID=SERVER.ID "
-				+ "GROUP BY SERVER.ENTRYDATE, SERVER.ID) AS SUBQUERY "
-				+ "WHERE PAGES = 1 AND " + filter.getSql() + ";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Bounces",results.getString(1)));
-
-		results = conn.createStatement().executeQuery("SELECT COUNT(*) AS Frequency, * FROM"
-				+ "(SELECT IMPRESSIONS.*, CLICKS.* FROM IMPRESSIONS"
-				+ " INNER JOIN CLICKS ON IMPRESSIONS.ID=CLICKS.ID"
-				+ " GROUP BY CLICKS.DATE, CLICKS.ID) AS SUBQUERY"
-				+ " WHERE " + filter.getSql() + ";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Clicks",results.getString(1)));
-
-		results = conn.createStatement().executeQuery("SELECT COUNT(*) AS Frequency, * "
-				+ "FROM (SELECT IMPRESSIONS.*, SERVER.* FROM "
-				+ "IMPRESSIONS INNER JOIN SERVER ON IMPRESSIONS.ID=SERVER.ID "
-				+ "GROUP BY SERVER.ENTRYDATE, SERVER.ID) AS SUBQUERY "
-				+ "WHERE CONVERSION = 1 AND " + filter.getSql() + ";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Conversions",results.getString(1)));
-
-		results = conn.createStatement().executeQuery("SELECT COUNT(*) AS Frequency, * FROM IMPRESSIONS WHERE " +  filter.getSql() +";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Impressions",results.getString(1)));
-
-		results = conn.createStatement().executeQuery("SELECT COUNT(DISTINCT ID) AS Frequency, * FROM"
-				+ "(SELECT IMPRESSIONS.*, CLICKS.* FROM IMPRESSIONS"
-				+ " INNER JOIN CLICKS ON IMPRESSIONS.ID=CLICKS.ID"
-				+ " GROUP BY CLICKS.DATE, CLICKS.ID) AS SUBQUERY"
-				+ " WHERE " +  filter.getSql() + ";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Unique Clicks",results.getString(1)));
-		
-		results = conn.createStatement().executeQuery("SELECT COUNT(DISTINCT ID) AS Frequency, * FROM IMPRESSIONS WHERE " +  filter.getSql() +";");
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Unique Impressions",results.getString(1)));
-
-		results = conn.createStatement().executeQuery("SELECT CLICKCOST, IMPCOST FROM "
-				+ "(SELECT CLICKDATE, SUM(CLICKCOST) AS CLICKCOST FROM "
-				+ "(SELECT IMPRESSIONS.*, CLICKS.DATE AS CLICKDATE, CLICKS.ID, CLICKS.COST AS CLICKCOST "
-				+ "FROM IMPRESSIONS "
-				+ "INNER JOIN CLICKS "
-				+ "ON IMPRESSIONS.ID=CLICKS.ID "
-				+ "GROUP BY CLICKS.DATE, CLICKS.ID)"
-				+ "WHERE " +  filter.getSql().replace("DATE","CLICKDATE") + ") "
-				+ "INNER JOIN "
-				+ "(SELECT  SUM(COST) AS IMPCOST FROM IMPRESSIONS "
-				+ "WHERE " +  filter.getSql() + ")");
-		
-
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Total Cost",Integer.toString(results.getInt(1)+results.getInt(2))));
-		
-		results = conn.createStatement().executeQuery("SELECT NUMCLICKS, NUMIMP FROM "
-				+ "(SELECT SUBSTR(DATE, 0, 14) as CLICKDATE, COUNT(*) AS NUMCLICKS FROM "
-				+ "(SELECT CLICKS.DATE AS CLICKDATE, IMPRESSIONS.* FROM CLICKS INNER JOIN IMPRESSIONS ON CLICKS.ID=IMPRESSIONS.ID GROUP BY CLICKS.ID, CLICKDATE) "
-				+ "WHERE " +  filter.getSql().replace("DATE","CLICKDATE") + ") "
-				+ "INNER JOIN "
-				+ "(SELECT SUBSTR(DATE, 0, 14) AS DATE, COUNT(*) AS NUMIMP FROM IMPRESSIONS WHERE " +  filter.getSql() + ") "
-				+ "ON DATE=CLICKDATE");
-		
-		if (results.next()) 
-			tableContent.add(new dashboard.model.ObservableMetrics("CTR",Float.toString(results.getInt(1)/(float)results.getInt(2))));
-		
-		
-		results = conn.createStatement().executeQuery("SELECT SUM(CLICKCOST), SUM(IMPCOST), SUM(Frequency) FROM "
-				+ "(SELECT SUBSTR(ENTRYDATE, 0, 14) AS ENTRYDATE,COUNT(*) AS Frequency "
-				+ "FROM (SELECT IMPRESSIONS.*, SERVER.* FROM "
-				+ "IMPRESSIONS INNER JOIN SERVER ON IMPRESSIONS.ID=SERVER.ID "
-				+ "GROUP BY SERVER.ENTRYDATE, SERVER.ID) AS SUBQUERY "
-				+ "WHERE CONVERSION = 1 "
-				+ "GROUP BY SUBSTR(ENTRYDATE, 0, 14))"
-				+ "INNER JOIN "
-				+ "(SELECT CLICKDATE, CLICKCOST, IMPCOST FROM "
-				+ "(SELECT SUBSTR(CLICKDATE, 0, 14) AS CLICKDATE, SUM(CLICKCOST) AS CLICKCOST FROM "
-				+ "(SELECT IMPRESSIONS.*, CLICKS.ID, CLICKS.DATE AS CLICKDATE, CLICKS.COST AS CLICKCOST "
-				+ "FROM IMPRESSIONS "
-				+ "INNER JOIN CLICKS "
-				+ "ON IMPRESSIONS.ID=CLICKS.ID "
-				+ "GROUP BY CLICKS.DATE, CLICKS.ID) "
-				+ "GROUP BY SUBSTR(CLICKDATE, 0, 14)) "
-				+ "INNER JOIN "
-				+ "(SELECT SUBSTR(DATE,0,14) AS IMPDATE, SUM(COST) AS IMPCOST FROM IMPRESSIONS "
-				+ "GROUP BY SUBSTR(DATE, 0, 14)) "
-				+ "ON IMPDATE=CLICKDATE) "
-				+ "ON CLICKDATE=ENTRYDATE;");
-		
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("CPA",Float.toString((results.getInt(1) + results.getInt(2)) / (float)results.getInt(3))));
-		
-		results = conn.createStatement().executeQuery("SELECT SUM(CLICKCOST), SUM(IMPCOST), SUM(NUMCLICKS) FROM "
-				+ "(SELECT SUBSTR(CLICKDATE, 0, 14) AS CLICKDATE, SUM(CLICKCOST) AS CLICKCOST, COUNT(ID) AS NUMCLICKS FROM "
-				+ "(SELECT IMPRESSIONS.*, CLICKS.ID, CLICKS.DATE AS CLICKDATE, CLICKS.COST AS CLICKCOST "
-				+ "FROM IMPRESSIONS "
-				+ "INNER JOIN CLICKS "
-				+ "ON IMPRESSIONS.ID=CLICKS.ID "
-				+ "GROUP BY CLICKS.DATE, CLICKS.ID) "
-				+ "WHERE " + filter.getSql().replace("DATE", "CLICKDATE")+ " GROUP BY SUBSTR(CLICKDATE, 0, 14)) "
-				+ "INNER JOIN "
-				+ "(SELECT SUBSTR(DATE,0,14) AS IMPDATE, SUM(COST) AS IMPCOST FROM IMPRESSIONS "
-				+ "WHERE " + filter.getSql()+ " GROUP BY SUBSTR(DATE, 0, 14)) "
-				+ "ON IMPDATE=CLICKDATE");
-		
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("CPC",Float.toString((results.getInt(1) + results.getInt(2)) / (float)results.getInt(3))));
-		
-		results = conn.createStatement().executeQuery("SELECT AVG(COST) AS IMPCOST FROM IMPRESSIONS "
-				+ "WHERE " + filter.getSql());
-		
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("CPM",Float.toString(results.getFloat(1)*1000)));
-		
-		results = conn.createStatement().executeQuery("SELECT SUM(NUMCLICKS), SUM(NUMBOUNCES) FROM "
-				+ "(SELECT SUBSTR(CLICKDATE, 0, 14) as CLICKDATE, COUNT(ID) AS NUMCLICKS FROM "
-				+ "(SELECT CLICKS.DATE AS CLICKDATE, IMPRESSIONS.* FROM CLICKS INNER JOIN IMPRESSIONS ON CLICKS.ID=IMPRESSIONS.ID GROUP BY CLICKS.ID, CLICKDATE) "
-				+ "WHERE " + filter.getSql().replace("DATE", "CLICKDATE")+ " GROUP BY SUBSTR(CLICKDATE, 0, 14)) "
-				+ "INNER JOIN "
-				+ "(SELECT SUBSTR(ENTRYDATE, 0, 14) AS DATE, COUNT(*) AS NUMBOUNCES FROM "
-				+ "(SELECT IMPRESSIONS.*, SERVER.* FROM IMPRESSIONS "
-				+ "INNER JOIN SERVER ON IMPRESSIONS.ID=SERVER.ID "
-				+ "GROUP BY SERVER.ENTRYDATE, SERVER.ID) AS SUBQUERY "
-				+ "WHERE PAGES=1 AND "+ filter.getSql().replace("DATE", "ENTRYDATE")
-				+ " GROUP BY SUBSTR(ENTRYDATE, 0, 14)) "
-				+ "ON DATE=CLICKDATE GROUP BY DATE");
-		
-		if (results.next())
-			tableContent.add(new dashboard.model.ObservableMetrics("Bounce Rate",String.format("%.1f%%", results.getInt(2)/results.getFloat(1)*100)));
-		
-		results.close();
-	}
-	private void drawGraph(String metric) {
-		//lineChart.getData().clear();
-		//lineChart.getXAxis().setLabel(filterTime.getValue());  
-		lineChart.getYAxis().setLabel("Number");
-		GraphConstructor constructor;
-
-		switch(metric) {
-			default:
-			case "Bounces":
-				constructor = new BounceGraphConstructor(filter);
-				break;
-			case "Impressions":
-				constructor = new ImpressionsGraphConstructor(filter);
-				break;
-			case "Clicks":
-				constructor = new ClicksGraphConstructor(filter);
-				break;
-			case "Unique Impressions":
-				constructor = new UniqueImpressionsGraphConstructor(filter);
-				break;
-			case "Unique Clicks":
-				constructor = new UniqueClicksGraphConstructor(filter);
-				break;
-			case "Conversions":
-				constructor = new ConversionGraphConstructor(filter);
-				break;
-			case "CPC":
-				constructor = new CPCGraphConstructor(filter);
-				break;
-			case "CPA":
-				constructor = new CPAGraphConstructor(filter);
-				break;
-			case "CPM":
-				constructor = new CPMGraphConstructor(filter);
-				break;
-			case "CTR":
-				constructor = new CTRGraphConstructor(filter);
-				break;
-			case "Total Cost":
-				constructor = new TotalCostGraphConstructor(filter);
-				break;
-			case "Bounce Rate":
-				constructor = new BounceRateGraphConstructor(filter);
-				break;
-		}
-
-		lineChart.setCreateSymbols(false);
-		//lineChart.setLegendVisible(false);
-		updateGraph(constructor, metric, lineChart);
-
-	}
 	
 	@FXML
 	private void generateData(ActionEvent event) {
@@ -353,10 +141,46 @@ public class AuctionController extends AnchorPane {
 		lineChart.getData().clear();
 		lineChart.getXAxis().setLabel(filterTime.getValue());  
 		lineChart.getYAxis().setLabel(filterMetrics.getValue());
-				
-		drawGraph(filterMetrics.getValue());
+
+		lineChart.getData().clear();
+		updateGraph(filterMetrics.getValue());
+		
+		// Cheap and nasty threading
+		new Thread(new MetricsUpdater(tableMetrics, filter)).start();;
 	}
 
+	private void updateGraph(String metric) {
+		lineChart.getYAxis().setLabel("Number");
+		GraphConstructor constructor;
+
+		switch(metric) {
+			default:
+			case "Bounces":            constructor = new BounceGraphConstructor(filter);            break;
+			case "Impressions":        constructor = new ImpressionsGraphConstructor(filter);       break;
+			case "Clicks":             constructor = new ClicksGraphConstructor(filter);            break;
+			case "Unique Impressions": constructor = new UniqueImpressionsGraphConstructor(filter); break;
+			case "Unique Clicks":      constructor = new UniqueClicksGraphConstructor(filter);      break;
+			case "Conversions":        constructor = new ConversionGraphConstructor(filter);        break;
+			case "CPC":                constructor = new CPCGraphConstructor(filter);               break;
+			case "CPA":                constructor = new CPAGraphConstructor(filter);               break;
+			case "CPM":                constructor = new CPMGraphConstructor(filter);               break;
+			case "CTR":                constructor = new CTRGraphConstructor(filter);               break;
+			case "Total Cost":         constructor = new TotalCostGraphConstructor(filter);         break;
+			case "Bounce Rate":        constructor = new BounceRateGraphConstructor(filter);        break;
+		}
+
+		lineChart.setCreateSymbols(false);
+		lineChart.setLegendVisible(true);
+		
+		try {
+			Series<Date, Number> data = constructor.fetchGraph();
+			data.setName(metric);
+			lineChart.getData().add(data);
+		} catch (SQLException e) {
+			System.err.println("Unable to fetch data from database: " + e.getMessage());
+		}
+	}
+	
 	@FXML
 	private void openCampaignAction(ActionEvent event) {
 		FileChooser fChooser = new FileChooser();
